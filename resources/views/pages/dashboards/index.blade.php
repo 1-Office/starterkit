c9d934151ab8e4fad8b442d22fa1acf590930825
ThinaTaliwe <thina.taliwe2@gmail.com>
2023-04-11 12:02:46 +0200

    Initialization

---
+++ resources/views/pages/dashboards/index.blade.php
@@ -0,0 +1,13 @@
+ <x-default-layout> 
+
+    <!--begin::Row-->
+    <div class="row g-5 g-xl-10 mb-5 mb-xl-10">
+        <!--begin::Col-->
+        <div class="col-xl-12">
+            @include('partials/widgets/tables/_widget-14')
+        </div>
+        <!--end::Col-->
+    </div>
+    <!--end::Row-->
+
+ </x-default-layout>
